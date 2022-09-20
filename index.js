05b501a5c57b2a061bb87c2fba59123ae8173863
dylan <dylandiamond34@gmail.com>
2022-09-20 16:02:56 -0600

    commit tree 6f66d089a39f5bcb9aa6fe251e1b1d93dede803a

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = (num1 * num2)
+
+const random = Math.floor(Math.random() + 1)
+
+let num3 = 3.3
+let num4 = .5
+
+const mod = Math.ceil(num3 + num4) 
+
+const max = Math.max(15,16,17,18,19,20)
+
+
+
